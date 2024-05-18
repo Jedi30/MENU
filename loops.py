7b06b7f943a938e581d3288bdd45e71e686a2eaa
Jedi30 <93566900+Jedi30@users.noreply.github.com>
2024-05-18 12:51:29 +0800

    Add files via upload

--- loops.py
+++ loops.py
@@ -7,12 +7,85 @@
 #     value += 1
     
 
-value = 1
-while value <=10:
-    value += 1
-    if value == 5:
-        continue
-    print(value)
-else:
-    print("the value is equal to " + str(value))
+# value = 1
+# while value <=10:
+#     value += 1
+#     if value == 5:
+#         continue
+#     print(value)
+# else:
+#     print("the value is equal to " + str(value))
 
+#for loops
+
+# value = 1
+# while value <=10:
+#     print(value)
+#     value+=1
+
+# value = 1
+# while value<10:
+#     print(value)
+#     if value == 5:
+#         break
+#     value+=1
+
+# names = ["Jerome", "May", "Chris"]
+
+# for x in names:
+#     print(x)
+
+# for x in "Mississippi":
+#     print(x)
+
+# value= 1
+# while value <= 10:
+#       print(value)
+#       value +=1
+
+# value = 1
+# while value <= 10:
+#       print(value)
+#       if value == 5:
+#          break    
+#       value+=1      
+
+# value = 1
+# while value <= 10:
+#     value += 1
+#     if value == 5:
+#         continue
+#         print(value)
+# else:
+#     print("The value is equal to  " + str(value))
+ 
+names = ['Chris', "May", "Jerome"]
+ 
+# for x in names:
+#     if x == 'May':
+#         break
+#     print(x)
+
+# for x in names:
+#     if x == "May":
+#      continue
+#     print(x)
+
+# for x in range(4):
+#    print(x)    
+
+# for x in range(2,4):
+#    print(x)   
+
+for x in range(5,101,5) :
+   print(x)  
+else:
+   print("Glad that its over!")
+
+names = ["Chris", "May", "Jerome"]
+actions =["Code", "eats", "Sleep", ]
+
+for name in names:
+    for action in actions:
+       print(name + " " + action + "")
+   
